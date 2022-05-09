24553dcaa21e296a71b2d9cf30b3a6b084c899fc
alkon134@gmail.com <Lelo132$>
2022-05-09 15:56:31 +0300

    Create pet service

---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/pet/PetService.java
@@ -0,0 +1,42 @@
+package com.udacity.jdnd.course3.critter.pet;
+
+import com.udacity.jdnd.course3.critter.entity.Customer;
+import com.udacity.jdnd.course3.critter.entity.Pet;
+import com.udacity.jdnd.course3.critter.user.CustomerRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PetService {
+
+    @Autowired
+    PetRepository petRepository;
+    @Autowired
+    CustomerRepository customerRepository;
+
+    public Pet save(PetDTO petDTO) {
+       Optional<Customer> customer =customerRepository.findById(petDTO.getOwnerId());
+       Pet pet = new Pet();
+        BeanUtils.copyProperties(petDTO,pet);
+        if (!customer.isEmpty()) {
+            pet.setCustomer(customer.get());
+        }
+       return petRepository.save(pet);
+    }
+
+    public Optional<Pet> getPet(long petId) {
+       return petRepository.findById(petId);
+    }
+
+    public List<Pet> getPets() {
+       return (List<Pet>) petRepository.findAll();
+    }
+
+    public List<Pet> getPetByOwner(long ownerId) {
+         return petRepository.findPetByCustomerId(ownerId);
+    }
+}
