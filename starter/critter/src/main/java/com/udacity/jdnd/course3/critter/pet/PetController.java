ba8e797e7788b27c0ed2e760026c6f396923fd88
alkon134@gmail.com <Lelo132$>
2022-05-09 15:56:00 +0300

    Create pet repository,service,entity

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/pet/PetController.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/pet/PetController.java
@@ -1,8 +1,14 @@
 package com.udacity.jdnd.course3.critter.pet;
 
+import com.udacity.jdnd.course3.critter.entity.Pet;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * Handles web requests related to Pets.
@@ -11,23 +17,50 @@ import java.util.List;
 @RequestMapping("/pet")
 public class PetController {
 
+    @Autowired
+    PetService petService;
+
     @PostMapping
     public PetDTO savePet(@RequestBody PetDTO petDTO) {
-        throw new UnsupportedOperationException();
+        Pet pet = new Pet();
+        BeanUtils.copyProperties(petDTO,pet);
+       pet = petService.save(petDTO);
+        BeanUtils.copyProperties(pet,petDTO);
+        return petDTO;
     }
 
     @GetMapping("/{petId}")
     public PetDTO getPet(@PathVariable long petId) {
-        throw new UnsupportedOperationException();
+       Optional<Pet> pet = petService.getPet(petId);
+       PetDTO petDTO = new PetDTO();
+        if (pet.get() != null) {
+            BeanUtils.copyProperties(pet.get(),petDTO);
+            return petDTO;
+        }
+       return null;
     }
 
     @GetMapping
-    public List<PetDTO> getPets(){
-        throw new UnsupportedOperationException();
+    public List<PetDTO> getPets() {
+        List<Pet> pets = petService.getPets();
+        List<PetDTO> petsDTO = new ArrayList<PetDTO>();
+        pets.stream().forEach(pet -> {
+            PetDTO petDTO = new PetDTO();
+            BeanUtils.copyProperties(pet, petDTO);
+            petsDTO.add(petDTO);
+        });
+        return petsDTO;
     }
 
     @GetMapping("/owner/{ownerId}")
     public List<PetDTO> getPetsByOwner(@PathVariable long ownerId) {
-        throw new UnsupportedOperationException();
+        List<Pet> pets = petService.getPetByOwner(ownerId);
+        List<PetDTO> petsDTO = new ArrayList<PetDTO>();
+        pets.stream().forEach(pet -> {
+            PetDTO petDTO = new PetDTO();
+            BeanUtils.copyProperties(pet, petDTO);
+            petsDTO.add(petDTO);
+        });
+         return petsDTO;
     }
 }
