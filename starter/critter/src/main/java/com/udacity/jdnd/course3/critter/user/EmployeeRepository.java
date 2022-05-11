7ff6b780eee9eb014b48c560c89731e340f9514d
alkon134@gmail.com <Lelo132$>
2022-05-11 04:36:32 +0300

    update query in findemployeebyskills

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/EmployeeRepository.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/EmployeeRepository.java
@@ -1,17 +1,16 @@
 package com.udacity.jdnd.course3.critter.user;
 
 import com.udacity.jdnd.course3.critter.entity.Employee;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
-import java.util.List;
 import java.util.Set;
 
 @Repository
 public interface EmployeeRepository extends CrudRepository<Employee,Long> {
-
-    List<Employee> findEmployeeBySkillsInAndDaysAvailable(Set<EmployeeSkill> skills, LocalDate date);
-
-    Set<Employee> findEmployeeBySkillsIn(Set<EmployeeSkill> skills);
+    @Query("SELECT e FROM Employee e LEFT JOIN e.skills sk WHERE sk IN :skillList"
+            + " GROUP BY e HAVING COUNT( sk) = :skillListSize" )
+    Set<Employee> findEmployeeByskills(@Param("skillList") Set<EmployeeSkill> skills, @Param("skillListSize") long skillListSize);
 }
